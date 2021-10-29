dafd7e543fb5056d3d6b3326b181926da672c57c
luttrelb <55104091+luttrelb@users.noreply.github.com>
2021-10-29 09:39:43 -0400

    Update ship.hpp

--- ship.hpp
+++ ship.hpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <string>
-#include <vector>
-using namespace std;
 
 enum ShipDirection {VERTICAL, HORIZONTAL};
 
 class Ship {
     public:
+        //default ship construction initializing default private variables
         Ship(){
             shipType = "";
             dir = VERTICAL;
@@ -15,6 +14,9 @@ class Ship {
             numOfHits = 0;
         }
 
+
+        //ship constructor that initializes the number of hits the instantiated ship can take,
+        //and the name of the ship (CARRIER, BATTLESHIP, SUBMARINE etc.)
         Ship(int numHits, string name){
             shipType = name;
             dir = VERTICAL;
@@ -23,42 +25,54 @@ class Ship {
             numOfHits = numHits;
         }
 
-        void setStartCol(int sc){
+        //setter for setting the initial column of the ship
+        void setCol(int sc){
             startCol = sc;
         }
 
-        const int getStartCol(){
+        //getter for the starting column of the ship
+        const int getCol(){
             return startCol;
         }
 
-         void setRow(int sr){
+        //setter for setting the initial column of the ship
+        void setRow(int sr){
             startRow = sr;
         }
 
-        const int getStartRow(){
+        //getter for the starting row of the ship
+        const int getRow(){
             return startRow;
         }
 
-         void setShipDirection(ShipDirection sd){
+        //setter that determines if the ship is positioned horizontal or vertical
+        void setDirection(ShipDirection sd){
             dir = sd;
         }
 
-        const int getNumHits(){
+        const ShipDirection getDirection(){
+            return dir;
+        }
+
+        //getter for getting the number of hits left of the ship
+        const int getHitNum(){
             return numOfHits;
         }
 
+        //getter for getting the name of the ship
         const string getShipName(){
             return shipType;
         }
 
+        //setter for setting the ship name
         void setShipName(string shipName){
             shipType = shipName;
         }
 
     private:
-      int numOfHits;
-      ShipDirection dir;
-      int startRow;
-      int startCol;
-      string shipType;
+      int numOfHits; //private variable for determining how many hits a boat can take
+      ShipDirection dir; //private variable determining horizontal or vertical positioning
+      int startRow; // private variable for the starting row of the boat
+      int startCol; // private variable for the starting column of the boat
+      string shipType; //private variable for the ship name/type
 }

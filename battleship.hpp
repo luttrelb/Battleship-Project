6a648fd069ff7bd5a07cf92121aa3d9e097d8f4c
luttrelb <55104091+luttrelb@users.noreply.github.com>
2021-11-01 10:28:35 -0400

    Update battleship.hpp

--- battleship.hpp
+++ battleship.hpp
@@ -2,37 +2,50 @@
 #include <string>
 #include "board.hpp"
 
+#include <iostream>
+#include <string>
+#include "board.hpp"
+
+//enumeration used to evaluate vertical or horizontal position
 enum ShipDirection {VERTICAL, HORIZONTAL};
 
 class Battleship {
     private:
-        string player; //private variable used for switching turns (player)
-        string computer; //private variable used for switching turns (computer)
+        string winner; // holds the winner
 
     public:
-        //set player function for swapping to player functionality
-        void setPlayer(string gamePlayer){
-            this->gamePlayer = player;
+    
+       /**
+        * function for setting winner of the game
+        *
+        * @return returns the winner of the game
+        */
+        //return winner
+        string getWinner(){
+            std::cout << "The winner is: " + winner + "!";
+            return winner;
         }
 
-        //set player function for swapping to computer functionality
-        void setComputer(string gameComputer){
-            this->gameComputer = computer;
-        }
-
-        //function that checks if the game has been won yet
-        const bool checkGameState(Board board){
+        /**
+        * setter for setting the initial row of the ship
+        *
+        * @param board the board that is being checked if in win state
+        * @param currentTurn the turn of the player, used to set win state if current turn has won
+        * @return returns true if game has been won, false otherwise
+        */
+        const bool checkGameState(Board board, string currentTurn){
             //for loop with nested for loop that checks all spaces for 1s, 2s, 3s, 4s, or 5s
             //finding any of these values indicates game is still going
             for(int row = 0; row < 10; row++){
 				for(int col = 0; col < 10; col++){
-					if((board[row][col] = 1) || (board[row][col] = 2) || (board[row][col] = 3 || (board[row][col] = 1) || 4)
+					if((board[row][col] = 1) || (board[row][col] = 2) || (board[row][col] = 3 || (board[row][col] = 4)
                     || (board[row][col] = 5)){
 					    return false;
 					}
                 }
             }
             //return true otherwise
+            winner = currentTurn;
 			return true;
         }
 }

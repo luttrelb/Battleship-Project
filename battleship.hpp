ff49de4703ec9596c673c9008e895321d109b751
luttrelb <55104091+luttrelb@users.noreply.github.com>
2021-11-08 08:36:14 -0500

    Update battleship.hpp

--- battleship.hpp
+++ battleship.hpp
@@ -1,14 +1,7 @@
-#include <iostream>
-#include <string>
-#include "board.hpp"
-
-//enumeration used to evaluate vertical or horizontal position
-enum ShipDirection {VERTICAL, HORIZONTAL};
-
 class Battleship {
     private:
-        string winner; // holds the winner
-
+        std::string winner; // holds the winner
+        std::string currentTurn;
     public:
     
        /**
@@ -17,7 +10,7 @@ class Battleship {
         * @return returns the winner of the game
         */
         //return winner
-        string getWinner(){
+        std::string getWinner(){
             std::cout << "The winner is: " + winner + "!";
             return winner;
         }
@@ -29,12 +22,12 @@ class Battleship {
         * @param currentTurn the turn of the player, used to set win state if current turn has won
         * @return returns true if game has been won, false otherwise
         */
-        const bool checkGameState(Board board, string currentTurn){
+        const bool checkGameState(Board board[10][10], std::string currentTurn){
             //for loop with nested for loop that checks all spaces for 1s, 2s, 3s, 4s, or 5s
             //finding any of these values indicates game is still going
             for(int row = 0; row < 10; row++){
 				for(int col = 0; col < 10; col++){
-					if((board[row][col] = 1) || (board[row][col] = 2) || (board[row][col] = 3 || (board[row][col] = 4)
+					if((board[row][col] == 1) || (board[row][col] == 2) || (board[row][col] = 3 )|| (board[row][col] = 4)
                     || (board[row][col] = 5)){
 					    return false;
 					}
@@ -44,4 +37,4 @@ class Battleship {
             winner = currentTurn;
 			return true;
         }
-}
+};
